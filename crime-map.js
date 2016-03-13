5ea23971377d7c95c7c5999cf103574228391771
WingYee Mak <wwymak@gmail.com>
2016-03-13 15:02:52 +0000

    some extra commnets

--- crime-map.js
+++ crime-map.js
@@ -1,26 +1,38 @@
-var width = 960,
+(function(){
+    //svg dimensions
+    var width = 960,
     height = 500;
 
-var svg = d3.select("#hexmap").append("svg").attr("width", width).attr("height", height);
+    var svg = d3.select("#hexmap").append("svg").attr("width", width).attr("height", height);
 
-var hexbin = d3.hexbin()
-    .size([width, height])
-    .radius(3)
-    .x(function(d){return d.p0}).y(function(d){return d.p1});
+    var hexbin = d3.hexbin()
+        .size([width, height])
+        .radius(3)
+        .x(function(d){return d.p0}).y(function(d){return d.p1});
 
-var colorScale = d3.scale.linear()
-    .range(['#FFFFB2', '#B10026'])
-    .interpolate(d3.interpolateLab);
+    var colorScale = d3.scale.linear()
+        .range(['#FFFFB2', '#B10026'])
+        .interpolate(d3.interpolateLab);
 
-var categoryColors = d3.scale.category20c();
+    //for assigning a color per crime cateogory
+    var categoryColors = d3.scale.category20c();
 
-var hexBinnedData;
-queue().defer(d3.json, "outer-london-topojson.json")
-    .defer(d3.csv, "2015-06-crime-filtered.csv")
-    .defer(d3.json, "crime-categories.json")
-    .await(makeMyMap);
+    var hexBinnedData;
 
-function groupByType(array){
+    //grab the london topojson to draw as well as the crime data
+    queue().defer(d3.json, "outer-london-topojson.json")
+        .defer(d3.csv, "2015-06-crime-filtered.csv")
+        .defer(d3.json, "crime-categories.json")
+        .await(makeMyMap);
+
+  /**
+   * basically, for each hexagon area, you have a series of data corresponding to
+   * each individual  crime that happened in that time period
+   * this groups them by their keys and counts how many of each category there is
+   * @param array
+   * @returns {Array}
+   */
+  function groupByType(array){
     var newArr = [], categories = {};
     for(var i = 0; i< array.length; i++){
         var cur = array[i];
@@ -33,89 +45,103 @@ function groupByType(array){
     var keys =  Object.keys(categories);
     keys.forEach(function(d){
         newArr.push(categories[d])
-    })
+    });
     return newArr;
 }
 
-function makeMyMap(err, topojsonData, crimeData, crimeCategories){
+    //draw the map after all data fetched
+    function makeMyMap(err, topojsonData, crimeData, crimeCategories){
 
-    var geoData = topojson.feature(topojsonData, topojsonData.objects["outer-london.geo"])//.features
+        var geoData = topojson.feature(topojsonData, topojsonData.objects["outer-london.geo"])
+        //set the projection to center on london
+        var projection = d3.geo.albers()
+            .center([4.5, 51.5])
+            .rotate([4.4, 0])
+            .parallels([50, 60])
+            .scale(50000)
+            .translate([width / 2, height / 2]);
 
-    var projection = d3.geo.albers()
-        .center([4.5, 51.5])
-        .rotate([4.4, 0])
-        .parallels([50, 60])
-        .scale(50000)
-        .translate([width / 2, height / 2]);
+        var geopath = d3.geo.path().projection(projection);
 
-    var geopath = d3.geo.path().projection(projection);
+        //draw the london boundary with geo.albers
+        //this is more a guide to the eye than anything else to be honest
+        var boundary = topojson.mesh(topojsonData, topojsonData.objects["outer-london.geo"])
 
-    var boundary = topojson.mesh(topojsonData, topojsonData.objects["outer-london.geo"])
+        svg.append("path").datum(boundary)
+            .attr("class", "boundary")
+            .attr("d", geopath);
 
-    svg.append("path").datum(boundary)
-        .attr("class", "boundary")
-        .attr("d", geopath);
-
-    crimeData.forEach(function(d){
-        var p = projection([d.Longitude, d.Latitude]);
-        d.p0 = p[0];
-        d.p1 = p[1];
-    });
-
-    var hexbinned = hexbin(crimeData);
-    hexBinnedData = hexbinned;
-    var crimeCatList = [];
-    for (var i = 0; i< crimeCategories.length; i++){
-        var item = crimeCategories[i]
-        var temp = {};
-        temp.name = item.name;
-        temp.color = categoryColors(i)
-            crimeCatList.push(temp);
-    }
-
-    var lengthArr = []
-    hexbinned.forEach(function(d){
-        lengthArr.push(d.length)
-    });
-    var maxNumber = d3.max(lengthArr);
-
-    hexbinned.forEach(function(bin){
-        bin.groupedData = groupByType(bin);
-        var sorted = bin.groupedData.sort(function(a, b){
-            return b.count - a.count;
+        //convert the lat and lngs of the crime data to svg coords
+        crimeData.forEach(function(d){
+            var p = projection([d.Longitude, d.Latitude]);
+            d.p0 = p[0];
+            d.p1 = p[1];
         });
-        bin.maxType = sorted[0].type;
-        bin.maxCount = sorted[0].count;
 
-    });
+        //bin the crime data into geo hexagons
+        var hexbinned = hexbin(crimeData);
+        hexBinnedData = hexbinned;
+        var crimeCatList = [];
+        for (var i = 0; i< crimeCategories.length; i++){
+            var item = crimeCategories[i]
+            var temp = {};
+            temp.name = item.name;
+            temp.color = categoryColors(i)
+                crimeCatList.push(temp);
+        }
 
-    colorScale.domain([0, Math.sqrt(maxNumber)])
+        //this section is for finding out the max crime per binned
+        //it's not used at the moment-- still figuring out what to do with it!
+        var lengthArr = []
+        hexbinned.forEach(function(d){
+            lengthArr.push(d.length)
+        });
+        var maxNumber = d3.max(lengthArr);
 
-    svg.append("g")
-        .attr("class", "hexagons")
-        .selectAll("path")
-        .data(hexbinned)
-        .enter().append("path")
-        .attr("d", function(d) { return hexbin.hexagon(); })
-        .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
-        //.style("fill", function(d) { return colorScale(Math.sqrt(d.length)); })
-        .style("fill", function(d) {
-            var crime = d.maxType;
-            return crimeCatList.filter(function(a){
-                return a.name == crime
-            })[0].color; });
+        hexbinned.forEach(function(bin){
+            bin.groupedData = groupByType(bin);
+            var sorted = bin.groupedData.sort(function(a, b){
+                return b.count - a.count;
+            });
+            //find out which crime is most dominant in each hexagon bin
+            //also how many crimes there is
+            bin.maxType = sorted[0].type;
+            bin.maxCount = sorted[0].count;
 
-    var drawLegendItem = function(color, dataName){
-        var legendList = $("#mapLegend >ul");
-        legendList.append('<li><div class="circle-div colorkey" style=background-color:' + color
-            + ' id="' + dataName + '" ></div><span class="data-title">'
-            + dataName + '</span></li>');
-    };
+        });
+
+        colorScale.domain([0, Math.sqrt(maxNumber)])
+
+        //add svg group, with class hexagon
+        svg.append("g")
+            .attr("class", "hexagons")
+            //data bind the hexagonally binned data to the hexagon path
+            .selectAll("path")
+            .data(hexbinned)
+            //draw the path
+            .enter().append("path")
+            //hexbin.hexagon returns the right path variable for hexagons for you
+            .attr("d", function(d) { return hexbin.hexagon(); })
+            .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+            //and the fill of each is the most predominant crime
+            .style("fill", function(d) {
+                var crime = d.maxType;
+                return crimeCatList.filter(function(a){
+                    return a.name == crime
+                })[0].color; });
+
+        var drawLegendItem = function(color, dataName){
+            var legendList = $("#mapLegend >ul");
+            legendList.append('<li><div class="circle-div colorkey" style=background-color:' + color
+                + ' id="' + dataName + '" ></div><span class="data-title">'
+                + dataName + '</span></li>');
+        };
+
+        //draw the color legend
+        crimeCatList.forEach(function(item){
+            drawLegendItem(item.color, item.name);
+        });
 
 
-    crimeCatList.forEach(function(item){
-        drawLegendItem(item.color, item.name)
-    })
-
-
-}
+    }
+})();
--- readme.md
+++ readme.md
@@ -1,10 +1,12 @@
 Basically trying to display london crime data as a hexbinned map, the color of each dot represents the most common crime in that area. Currently the data shows june2015
 
-NEEDS A LEGEND
+It makes use of the d3.hexbin plugin (https://github.com/d3/d3-plugins/tree/master/hexbin)
+to bin the data into hexagons. The most predominanat crime in each hexagon then 
+determines the color of that
 
 Crime data from police.uk
 
-TODOS/things to try: 
+Potential other Things to try: 
 
 * hover over a point and it shows a (piechart?) pf the breakdown of distribution of crime in that area
 
@@ -12,5 +14,11 @@ TODOS/things to try:
 
 * overlay the hexbin on top of leaflet (?) allow zooming and panning
 
-* show more time intervals/ try to use node(?) or something to hexbin the data server side (?) currently it's massive just for 1 month...
+* show more time intervals/ try to use node(?) or something to hexbin the data server side (?) 
+ currently it's massive just for 1 month...
+ 
+ Caveats: it is an _experiment_ so there's lots of optimisations that I didn't do, e.g. window resize 
+ handlers, a `reset projection` function so that the map scales with svg size, mouseover events etc
+ 
+ 
 
